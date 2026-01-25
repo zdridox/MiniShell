b6486a41311cf7a0f334a686a1e3a51af9971608
Maksym Melnyk <mamelnyk@c3r1s1.42warsaw.pl>
2026-01-25 05:36:24 +0100

    Secured malloc's

--- src/build_prompt.c
+++ src/build_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mamelnyk <mamelnyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 22:53:50 by mamelnyk          #+#    #+#             */
-/*   Updated: 2026/01/24 00:45:20 by mamelnyk         ###   ########.fr       */
+/*   Updated: 2026/01/24 23:19:53 by mamelnyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,13 @@ char	*build_prompt(t_shell *shell)
 	part1 = ft_strjoin(GREEN, " ➜ ");
 	part2 = ft_strjoin(BLUE, get_current_dir_name(shell));
 	part3 = ft_strjoin_three(ORANGE, " ✗ ", RESET);
+	if (!part1 || !part2 || !part3)
+	{
+		free(part1);
+		free(part2);
+		free(part3);
+		error_exit("Failed to build prompt", shell);
+	}
 	prompt = ft_strjoin_three(part1, part2, part3);
 	free(part1);
 	free(part2);
