d5842dbc2b224a795de8015b40f4b7a7865c8172
Maksym Melnyk <mamelnyk@c4r1s5.42warsaw.pl>
2026-04-24 04:46:43 +0200

    Started building AST parsing layers

--- src/parser_new.c
+++ src/parser_new.c
@@ -6,7 +6,7 @@
 /*   By: mamelnyk <mamelnyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 19:47:02 by mamelnyk          #+#    #+#             */
-/*   Updated: 2026/04/22 21:08:04 by mamelnyk         ###   ########.fr       */
+/*   Updated: 2026/04/23 22:55:48 by mamelnyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ bool	is_redirect(t_token_type type)
 {
 	return (type == REDIRECT_IN || type == REDIRECT_OUT
 		|| type == APPEND || type == HEREDOC);
+}
+
+bool	is_logical(t_token_type type)
+{
+	return (type == AND || type == OR);
 }
 
 t_redirect_type	token_type_to_redirect_type(t_token_type type)
@@ -30,7 +35,7 @@ t_redirect_type	token_type_to_redirect_type(t_token_type type)
 		return (REDIRECT_HEREDOC);
 }
 
-t_word	*create_word_from_token(t_token *token)
+t_word	*create_word_from_token(t_token **token)
 {
 	t_word		*word;
 
@@ -40,6 +45,7 @@ t_word	*create_word_from_token(t_token *token)
 	word->next = NULL;
 	// parer takes ownership of word_parts, so token should not free them
 	word->word_parts = token->word_parts;
+	(*token)->word_parts = NULL;
 	return (word);
 }
 
@@ -55,7 +61,7 @@ t_redirect_node	*parse_redirect(t_token **current_token)
 	*current_token = (*current_token)->next;
 	if ((*current_token)->type != WORD)
 		return (free(redirect_node), NULL);
-	target = create_word_from_token(*current_token);
+	target = create_word_from_token(current_token);
 	if (!target)
 		return (free(redirect_node), NULL);
 	redirect_type = token_type_to_redirect_type((*current_token)->type);
@@ -66,10 +72,230 @@ t_redirect_node	*parse_redirect(t_token **current_token)
 	return (redirect_node);
 }
 
+t_ast_node	*create_ast_node(t_node_type type, t_command *command, t_ast_node *left, t_ast_node *right)
+{
+	t_ast_node	*node;
+
+	node = malloc(sizeof(t_ast_node));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->command = command;
+	node->left = left;
+	node->right = right;
+	return (node);
+}
+
+t_node_type	token_type_to_node_type(t_token_type type)
+{
+	if (type == AND)
+		return (NODE_AND);
+	else if (type == OR)
+		return (NODE_OR);
+	else if (type == PIPE)
+		return (NODE_PIPE);
+	else
+		return (NODE_COMMAND);
+}
+
+void	add_word_to_end(t_word *head, t_word *new_word)
+{
+	t_word	*current;
+
+	current = head;
+	while (current->next)
+		current = current->next;
+	current->next = new_word;
+}
+
+bool	parse_one_word(t_token **current_token, t_word **words_head)
+{
+	t_word	*new_word;
+
+	new_word = create_word_from_token(current_token);
+	if (!new_word)
+		return (false);
+	if (!*words_head)
+		*words_head = new_word;
+	else
+		add_word_to_end(*words_head, new_word);
+	(*current_token) = (*current_token)->next;
+	return (true);
+}
+
+void	add_redirect_to_end(t_redirect_node *head, t_redirect_node *new_redirect)
+{
+	t_redirect_node	*current;
+
+	current = head;
+	while (current->next)
+		current = current->next;
+	current->next = new_redirect;
+}
+
+bool	parse_one_redirect(t_token **current_token, t_redirect_node **redirects_head)
+{
+	t_redirect_node	*new_redirect;
+
+	new_redirect = parse_redirect(current_token);
+	if (!new_redirect)
+		return (false);
+	if (!*redirects_head)
+		*redirects_head = new_redirect;
+	else
+		add_redirect_to_end(*redirects_head, new_redirect);
+}
+
+bool	parse_one_redirect(t_token **current_token, t_redirect_node **redirects_head)
+{
+	t_redirect_node	*new_redirect;
+	t_redirect_node	*current_redirect;
+
+	new_redirect = parse_redirect(current_token);
+	if (!new_redirect)
+		return (false);
+	if (!*redirects_head)
+	{
+		*redirects_head = new_redirect;
+		current_redirect = new_redirect;
+	}
+	else
+	{
+		current_redirect->next = new_redirect;
+		current_redirect = new_redirect;
+	}
+	return (true);
+}
+
+t_ast_node	*parse_command(t_token **current_token)
+{
+	t_ast_node		*command_node;
+	t_command		*command;
+	t_word			*words_head;
+	t_redirect_node	*redirects_head;
+
+	words_head = NULL;
+	redirects_head = NULL;
+	while ((*current_token)->type == WORD || is_redirect((*current_token)->type))
+	{
+		if ((*current_token)->type == WORD)
+		{
+			if (!parse_one_word(current_token, &words_head)
+				return (free_words(words_head), free_redirects(redirects_head), NULL);
+		}
+		if (is_redirect((*current_token)->type))
+		{
+			if (!parse_one_redirect(current_token, &redirects_head)
+				return (free_words(words_head), free_redirects(redirects_head), NULL);
+		}
+	}
+	command_node = create_ast_node(NODE_COMMAND, NULL, NULL, NULL);
+	if (!command_node)
+		return (free_words(words_head), free_redirects(redirects_head), NULL);
+	command = create_command(words_head, redirects_head);
+	if (!command)
+		return (free(command_node), free_words(words_head), free_redirects(redirects_head), NULL);
+	command_node->command = command;
+	return (command_node);
+}
+
+t_ast_node	*parse_in_parentheses(t_token **current_token)
+{
+	t_ast_node	*group_node;
+
+	(*current_token) = (*current_token)->next; // skip LPAREN
+	group_node = parse_logical(current_token);
+	if (!group_node)
+		return (NULL);
+	if (!*current_token || (*current_token)->type != RPAREN)
+		return (free_ast(group_node), NULL);
+	(*current_token) = (*current_token)->next; // skip RPAREN
+	return (group_node);
+}
+
+t_ast_node	*parse_primary(t_token **current_token)
+{
+	t_ast_node	*group_node;
+	t_ast_node	*command_node;
+	
+	if (*current_token && (*current_token)->type == LPAREN)
+	{
+		group_node = parse_in_parentheses(current_token);
+		if (!logical_node)
+			return (NULL);
+		return (node);
+	}
+	else if (*current_token && (*current_token)->type == WORD)
+	{
+		command_node = parse_command(current_token);
+		if (!command_node)
+			return (NULL);
+		return (command_node);
+	}
+	else
+		return (NULL);
+}
+
+t_ast_node	*parse_pipeline(t_token **current_token)
+{
+	t_ast_node	*pipeline_node;
+	t_ast_node	*left;
+	t_ast_node	*right;
+
+	left = parse_primary(current_token);
+	if (!left)
+		return (NULL);
+	if (*current_token && (*current_token)->type == PIPE)
+	{
+		(*current_token) = (*current_token)->next;
+		right = parse_primary(current_token);
+		if (!right)
+			return (free_ast(left), NULL);
+		pipeline_node = create_logical_node(NODE_PIPE, NULL, left, right);
+		if (!logical_node)
+			return (free_ast(left), free_ast(right), NULL);
+		return (pipeline_node);
+	}
+	else
+		return (left);
+}
+
+t_ast_node	*parse_logical(t_token **current_token)
+{
+	t_ast_node	*logical_node;
+	t_ast_node	*left;
+	t_ast_node	*right;
+	t_node_type	node_type;
+
+	left = parse_pipeline(current_token);
+	if (!left)
+		return (NULL);
+	if (*current_token && is_logical((*current_token)->type))
+	{
+		(*current_token) = (*current_token)->next;
+		right = parse_pipeline(current_token);
+		if (!right)
+			return (free_ast(left), NULL);
+		node_type = token_type_to_node_type((*current_token)->type);
+		logical_node = create_logical_node(node_type, NULL, left, right);
+		if (!logical_node)
+			return (free_ast(left), free_ast(right), NULL);
+		return (logical_node);
+	}
+	else
+		return (left);
+}
+
 t_ast_node	*parse_tokens(t_token *tokens)
 {
 	t_ast_node	*root;
 	t_ast_node	*current;
 
-
+	root = NULL;
+	current = NULL;
+	while (tokens)
+	{
+		root = parse_logical(&tokens);
+	}
+	return (root);
 }
