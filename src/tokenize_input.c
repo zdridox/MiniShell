fd47d1b202b9dbfd2387424d7e819f13086d6394
Maksym Melnyk <mamelnyk@c3r4s2.42warsaw.pl>
2026-07-24 17:24:47 +0200

    Improved readability

--- src/tokenize_input.c
+++ src/tokenize_input.c
@@ -6,7 +6,7 @@
 /*   By: mamelnyk <mamelnyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 19:45:05 by mamelnyk          #+#    #+#             */
-/*   Updated: 2026/07/24 16:38:23 by mamelnyk         ###   ########.fr       */
+/*   Updated: 2026/07/24 17:13:42 by mamelnyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,9 @@
 bool	add_next_token(char *input, int *i, t_token **current)
 {
 	if (is_operator(input[*i]))
-	{
-		if (!add_operator_token(input, i, current))
-			return (false);
-	}
+		return (add_operator_token(input, i, current));
 	else
-	{
-		if (!add_word_token(input, i, current))
-			return (false);
-	}
-	return (true);
+		return (add_word_token(input, i, current));
 }
 
 t_token	*tokenize_input(char *input)
