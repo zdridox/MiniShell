3bfd18bf7ddc17fa9f32d86d13341f96bf737f53
Maksym Melnyk <mamelnyk@c3r4s2.42warsaw.pl>
2026-07-24 16:45:34 +0200

    Fixed norm in str_arr_utilities.c

--- src/str_arr_utilities.c
+++ src/str_arr_utilities.c
@@ -6,20 +6,19 @@
 /*   By: anatoliy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 15:20:34 by anatoliy          #+#    #+#             */
-/*   Updated: 2026/01/24 19:53:21 by anatoliy         ###   ########.fr       */
+/*   Updated: 2026/07/24 16:44:41 by mamelnyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h" // narazie spada
-#include "../libft/libft.h"
+#include "libft.h"
 
-void free_str_arr(char **str_arr)
+void	free_str_arr(char **str_arr)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
 	if (!str_arr)
-		return;
+		return ;
 	while (str_arr[i])
 	{
 		free(str_arr[i++]);
@@ -27,9 +26,9 @@ void free_str_arr(char **str_arr)
 	free(str_arr);
 }
 
-int str_arr_len(char **str_arr)
+int	str_arr_len(char **str_arr)
 {
-	unsigned int len;
+	unsigned int	len;
 
 	if (!str_arr)
 		return (0);
@@ -41,11 +40,11 @@ int str_arr_len(char **str_arr)
 	return (len);
 }
 
-char **copy_arr(char **old_str_arr)
+char	**copy_arr(char **old_str_arr)
 {
-	char **new_str_arr;
-	unsigned int new_arr_size;
-	unsigned int i;
+	char			**new_str_arr;
+	unsigned int	new_arr_size;
+	unsigned int	i;
 
 	if (!old_str_arr)
 		return (NULL);
@@ -68,89 +67,10 @@ char **copy_arr(char **old_str_arr)
 	return (new_str_arr);
 }
 
-// char **resize_str_arr(char **old_str_arr, int new_size) // possible leak? // cos mieszalem
-// {
-// 	unsigned int i;
-// 	char **new_str_arr;
-
-// 	if (new_size < str_arr_len(old_str_arr))
-// 	{
-// 		return (NULL);
-// 	}
-// 	new_str_arr = (char **)malloc(new_size * sizeof(char *));
-// 	if (!new_str_arr)
-// 		return (NULL);
-// 	i = 0;
-// 	while (old_str_arr[i])
-// 	{
-// 		new_str_arr[i] = old_str_arr[i];
-// 		i++;
-// 	}
-// 	new_str_arr[i] = NULL;
-// 	free(old_str_arr);
-// 	return (new_str_arr);
-// }
-
-// char **resize_str_arr(char **old_str_arr, int new_size)
-// {
-// 	int i;
-// 	char **new_arr;
-
-// 	if (!old_str_arr)
-// 	{
-// 		new_arr = malloc(sizeof(char *) * 2);
-// 		new_arr[0] = NULL;
-// 		return (new_arr);
-// 	}
-// 	i = 0;
-// 	new_arr = malloc(sizeof(char *) * new_size + 1);
-// 	while (old_str_arr[i])
-// 	{
-// 		new_arr[i] = ft_strdup(old_str_arr[i]);
-// 		++i;
-// 	}
-// 	new_arr[i] = NULL;
-// 	free_str_arr(old_str_arr);
-// 	return (new_arr);
-// }
-
-char **resize_str_arr(char **old_str_arr, int new_size)
+char	**add_back_str_arr(char **str_arr, char *new_element)
 {
-	int i;
-	char **new_arr;
-
-	// new_size is the number of string elements, so add 1 for NULL terminator.
-	new_arr = malloc(sizeof(char *) * (new_size + 1));
-	if (!new_arr)
-	{
-		free_str_arr(old_str_arr); // Free old array on allocation failure
-		return (NULL);
-	}
-	i = 0;
-	if (old_str_arr)
-	{
-		while (old_str_arr[i] && i < new_size)
-		{
-			new_arr[i] = ft_strdup(old_str_arr[i]);
-			// It's good practice to check ft_strdup's return value
-			i++;
-		}
-	}
-	// Set the rest of the pointers, including the last one, to NULL.
-	while (i <= new_size)
-	{
-		new_arr[i] = NULL;
-		i++;
-	}
-	if (old_str_arr)
-		free_str_arr(old_str_arr);
-	return (new_arr);
-}
-
-char **add_back_str_arr(char **str_arr, char *new_element)
-{
-	char **new_str_arr;
-	int i;
+	char	**new_str_arr;
+	int		i;
 
 	new_str_arr = (char **)malloc((str_arr_len(str_arr) + 2) * sizeof(char *));
 	if (!str_arr)
@@ -172,13 +92,13 @@ char **add_back_str_arr(char **str_arr, char *new_element)
 	return (free_str_arr(str_arr), new_str_arr);
 }
 
-void print_str_arr(char **str_arr)
+void	print_str_arr(char **str_arr)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!str_arr)
-		return;
+		return ;
 	while (str_arr[i])
 	{
 		ft_putstr(str_arr[i++]);
