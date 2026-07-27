db44236c8d44aca22ba57b4875b359953d1c5904
Maksym Melnyk <mamelnyk@c3r12s2.42warsaw.pl>
2026-07-27 17:18:06 +0200

    Fixed norm in init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: mamelnyk <mamelnyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 20:23:22 by mamelnyk          #+#    #+#             */
-/*   Updated: 2026/06/24 14:08:06 by mamelnyk         ###   ########.fr       */
+/*   Updated: 2026/07/27 17:15:28 by mamelnyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 t_our_command	*init_our_commands(void)
 {
-	static t_our_command	our_commands[] = {
-		{"cd", &cd_command, false},
-		{"exit", &exit_command, false},
-		{"env", &env_command, true},
-		{"pwd", &pwd_command, true},
+	static t_our_command	our_commands[]
+		= {{"cd", &cd_command, false},
+	{"exit", &exit_command, false},
+	{"env", &env_command, true},
+	{"pwd", &pwd_command, true},
 		//{"echo", &echo_command},
 		//{"export", &export_command},
 		//{"unset", &unset_command},
-		{NULL, NULL, false}
-	};
+	{NULL, NULL, false}};
 
 	return (our_commands);
 }
 
-void	init_shell_with_null(t_shell *shell)
+static void	init_shell_with_null(t_shell *shell)
 {
 	shell->env = NULL;
 	shell->our_commands = NULL;
@@ -43,6 +42,8 @@ t_shell	*init_shell(char **envp)
 		error_exit("Failed to allocate shell", NULL);
 	init_shell_with_null(shell);
 	shell->env = copy_arr(envp);
+	if (shell->env == NULL)
+		error_exit("Failed to copy environment variables", shell);
 	shell->our_commands = init_our_commands();
 	shell->last_exit_code = 0;
 	if (shell->env == NULL || shell->our_commands == NULL)
